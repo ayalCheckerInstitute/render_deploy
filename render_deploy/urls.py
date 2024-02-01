fa1d8c46bbb1dae10bb0c8e5fd0448fcd5fc070a
Ayal Ganem <ayalganem55@gmail.com>
2024-02-01 14:25:46 +0200

    c

--- render_deploy/urls.py
+++ render_deploy/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from render_deploy.views import health_check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
 ]
