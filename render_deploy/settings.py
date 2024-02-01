17e72eab8e97906d4f0ebaa4eb7a686ae29e8507
Ayal Ganem <ayalganem55@gmail.com>
2024-02-01 14:58:26 +0200

    c

--- render_deploy/settings.py
+++ render_deploy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0*_)yt!o#l*!6-*wosw#n7-f#b#&p=)!lc@!=ennt9wz%nu!b=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME'), 'localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
